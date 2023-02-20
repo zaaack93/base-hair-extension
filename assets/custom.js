5793a96e5a2b90dc8b6c7c6409d529a96a60e2c9
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2023-02-20 15:25:09 +0100

    working on dispatch width resize

--- assets/custom.js
+++ assets/custom.js
@@ -10,12 +10,29 @@
 
 (function() {
   // Add custom code below this line
+  // Since there can be multiple elements with the class "popup-trigger", it returns an array. Putting the [0] will call the first button with the class "popup-trigger".
+  var formBuilderButton = document.getElementsByClassName("submit");
 
-
+  // Check for clicks on the button
+  console.log(formBuilderButton)
+  for(let formButton of formBuilderButton){
+    formButton[0].onclick = function(e) {
+      alert("clicked")
+      alert(e.target.getAttribute("data-modal"));
+    }
+  }
   
 
+  // document.dispatchEvent(
+  //   new CustomEvent('theme:resize', {
+  //     bubbles: true,
+  //   })
+  // )
 
 
+  document.getElementsByClassName("submit")[0].onclick=function(e){
+    alert("dd")
+  }
 
   // ^^ Keep your scripts inside this IIFE function call to 
   // avoid leaking your variables into the global scope.
